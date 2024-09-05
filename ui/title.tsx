0936953842e42333b586dcb35781c788e0fce476
Daniel Kappelle <daniel.kappelle@gmail.com>
2024-09-05 12:09:07 +0200

    Restyled title bar

--- ui/title.tsx
+++ ui/title.tsx
@@ -1,7 +1,7 @@
 export function Title({ title }: { title: string }) {
   return (
-    <div className="flex justify-center w-full bg-gray-400 mb-3">
-      <h1 className="font-bold">&#9660; {title} &#9660;</h1>
+    <div className="flex justify-center w-full bg-gray-300 mb-3 border border-black">
+      <h1 className="font-bold">{title}</h1>
     </div>
   );
 }
