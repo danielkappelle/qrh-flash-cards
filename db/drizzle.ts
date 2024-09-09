5560180a8310cb2ef1300b5775a37337b2d3b23e
Daniel Kappelle <daniel.kappelle@gmail.com>
2024-09-09 14:18:11 +0200

    Minor gitignore edit

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,7 +1,5 @@
-import { config } from 'dotenv';
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2';
-config({ path: '.env' });
 
 if (!process.env.DB_URL) {
   throw new Error('DB Credentials Error');
