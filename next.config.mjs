35965331f9e75372dd6224ec5d08214a3139b85c
Daniel Kappelle <daniel.kappelle@gmail.com>
2024-09-23 12:18:11 +0200

    Show package version on home screen

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+import pkg from './package.json' with { type: 'json' };
+
+console.log(12345, pkg.version);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
+  env: {
+    version: pkg.version,
+  },
 };
 
 export default nextConfig;
